32cee48e4396346ecd736807022f21a10300bd41
Mazai4ik <zheka78rus@yandex.ru>
2020-04-22 22:26:52 +0400

    laba4

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ include_once 'Tyreckix/Linear.php';
 include_once 'Tyreckix/Square.php';
 include_once 'Tyreckix/TyreckixException.php';
 include_once 'Tyreckix/Log.php';
+
+$arr=array();
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
+try {
+    $solver = new Tyreckix\Square();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    Tyreckix\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(Tyreckix\TyreckixException $e) {
+
+    Tyreckix\Log::log($e->getMessage());
+}
+Tyreckix\Log::write();
