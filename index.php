77db7948d207224793cfa48d98cba3177170bf22
Mazai4ik <zheka78rus@yandex.ru>
2020-04-22 22:25:00 +0400

    laba3

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once'core/EquationInterface.php';
+include_once'core/LogInterface.php';
+include_once'core/LogAbstract.php';
+include_once 'Tyreckix/Linear.php';
+include_once 'Tyreckix/Square.php';
+include_once 'Tyreckix/TyreckixException.php';
+include_once 'Tyreckix/Log.php';
